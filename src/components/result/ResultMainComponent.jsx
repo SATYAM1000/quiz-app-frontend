f5b74ea9730b7f13ebaeceb4cf8bc54f43571de0
satyamx <satyamsingh748846@gmail.com>
2024-08-09 14:08:07 +0530

    added disabled button

--- src/components/result/ResultMainComponent.jsx
+++ src/components/result/ResultMainComponent.jsx
@@ -29,10 +29,10 @@ export default function ResultPage() {
     <div className={`w-full flex flex-col lg:flex-row gap-20 pb-20`}>
       <div className={"w-full lg:flex-1"}>
         <div className="inline-flex flex-col items-start gap-2 relative flex-[0_0_auto]">
-          <div className="relative w-fit mt-[-1.00px] text-[48px] heading-l-regular font-[number:var(--heading-l-regular-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-regular-font-size)] tracking-[var(--heading-l-regular-letter-spacing)] leading-[var(--heading-l-regular-line-height)] whitespace-nowrap [font-style:var(--heading-l-regular-font-style)]">
+          <div className="relative w-fit mt-[-1.00px] text-[44px] heading-l-regular font-[number:var(--heading-l-regular-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-regular-font-size)] tracking-[var(--heading-l-regular-letter-spacing)] leading-[var(--heading-l-regular-line-height)] whitespace-nowrap [font-style:var(--heading-l-regular-font-style)]">
             Quiz Completed
           </div>
-          <div className="relative w-fit heading-l-bold text-[48px] font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-bold-font-size)] tracking-[var(--heading-l-bold-letter-spacing)] leading-[var(--heading-l-bold-line-height)] whitespace-nowrap [font-style:var(--heading-l-bold-font-style)]">
+          <div className="relative w-fit heading-l-bold text-[44px] font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-bold-font-size)] tracking-[var(--heading-l-bold-letter-spacing)] leading-[var(--heading-l-bold-line-height)] whitespace-nowrap [font-style:var(--heading-l-bold-font-style)]">
             Your Score...
           </div>
         </div>
