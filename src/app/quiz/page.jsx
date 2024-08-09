7b00cf0e716a29591d6dd5d23c4fa11ccff33be9
satyamx <satyamsingh748846@gmail.com>
2024-08-09 12:47:55 +0530

    done

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import data from "@/utils/data.json";
 import { useSearchParams } from "next/navigation";
 import QuizOptions from "@/components/quiz/QuizOptions";
@@ -56,62 +56,64 @@ export default function QuizPage() {
       : 0;
 
   return (
-    <section className={`w-full flex flex-col lg:flex-row gap-20 pb-20`}>
-      {/* --------LEFT SIDE----------- */}
-      <div className={`w-full lg:flex-1`}>
-        <div className="inline-flex flex-col lg:h-[452px] items-center justify-between relative flex-[0_0_auto]">
-          <div className="flex flex-col w-full items-start gap-[27px] relative flex-[0_0_auto]">
-            <div className="relative italic self-stretch mt-[-1.00px] text-[20px]">
-              Question {currentQuestionIndex + 1} of {quiz.questions.length}
+    <Suspense>
+      <section className={`w-full flex flex-col lg:flex-row gap-20 pb-20`}>
+        {/* --------LEFT SIDE----------- */}
+        <div className={`w-full lg:flex-1`}>
+          <div className="inline-flex flex-col lg:h-[452px] items-center justify-between relative flex-[0_0_auto]">
+            <div className="flex flex-col w-full items-start gap-[27px] relative flex-[0_0_auto]">
+              <div className="relative italic self-stretch mt-[-1.00px] text-[20px]">
+                Question {currentQuestionIndex + 1} of {quiz.questions.length}
+              </div>
+              <p className=" self-stretch font-heading-m font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] text-[36px] tracking-[var(--heading-m-letter-spacing)] leading-[var(--heading-m-line-height)] [font-style:var(--heading-m-font-style)] ">
+                {quiz.questions[currentQuestionIndex].question}
+              </p>
             </div>
-            <p className=" self-stretch font-heading-m font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] text-[36px] tracking-[var(--heading-m-letter-spacing)] leading-[var(--heading-m-line-height)] [font-style:var(--heading-m-font-style)] ">
-              {quiz.questions[currentQuestionIndex].question}
-            </p>
-          </div>
-          <div className="flex flex-col w-full mt-6 lg:mt-0 h-4 items-start justify-center gap-2 p-1 relative bg-white rounded-[999px]">
-            <div
-              className="relative h-2 bg-[#a729f5] rounded-[104px]"
-              style={{ width: `${progress}%` }}
-            />
+            <div className="flex flex-col w-full mt-6 lg:mt-0 h-4 items-start justify-center gap-2 p-1 relative bg-white rounded-[999px]">
+              <div
+                className="relative h-2 bg-[#a729f5] rounded-[104px]"
+                style={{ width: `${progress}%` }}
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* -----------RIGHT SIDE------------- */}
-      <div className={`w-full lg:flex-1 flex flex-col gap-6 mb-12 lg:mb-0`}>
-        {quiz.questions[currentQuestionIndex].options.map((option, index) => {
-          return (
-            <QuizOptions
-              key={index}
-              option={option}
-              currentIndex={index}
-              selectedOption={selectedOption}
-              setSelectedOption={setSelectedOption}
-              isUserAnswered={isUserAnswered}
-              isAnswerCorrect={isAnswerCorrect}
-              correctAnswer={quiz.questions[currentQuestionIndex].answer}
-            />
-          );
-        })}
+        {/* -----------RIGHT SIDE------------- */}
+        <div className={`w-full lg:flex-1 flex flex-col gap-6 mb-12 lg:mb-0`}>
+          {quiz.questions[currentQuestionIndex].options.map((option, index) => {
+            return (
+              <QuizOptions
+                key={index}
+                option={option}
+                currentIndex={index}
+                selectedOption={selectedOption}
+                setSelectedOption={setSelectedOption}
+                isUserAnswered={isUserAnswered}
+                isAnswerCorrect={isAnswerCorrect}
+                correctAnswer={quiz.questions[currentQuestionIndex].answer}
+              />
+            );
+          })}
 
-        <Button
-          name={
-            !isUserAnswered
-              ? "Submit Answer"
-              : isQuizCompleted
-              ? "Submit Quiz"
-              : "Next Question"
-          }
-          onClick={
-            !isUserAnswered
-              ? onAnswerSubmit
-              : isQuizCompleted
-              ? handleSubmitQuiz
-              : handleNextQuestion
-          }
-          disabled={!selectedOption && !isUserAnswered}
-        />
-      </div>
-    </section>
+          <Button
+            name={
+              !isUserAnswered
+                ? "Submit Answer"
+                : isQuizCompleted
+                ? "Submit Quiz"
+                : "Next Question"
+            }
+            onClick={
+              !isUserAnswered
+                ? onAnswerSubmit
+                : isQuizCompleted
+                ? handleSubmitQuiz
+                : handleNextQuestion
+            }
+            disabled={!selectedOption && !isUserAnswered}
+          />
+        </div>
+      </section>
+    </Suspense>
   );
 }
--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Button from "@/components/quiz/Button";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
@@ -19,60 +19,64 @@ export default function ResultPage() {
   if (!quiz) {
     return <div>Quiz not found</div>;
   }
-  
+
   const bgStyle = {
     backgroundColor: quiz?.backgroundColor,
     borderRadius: "0.4rem",
   };
 
   return (
-    <div className={`w-full flex flex-col lg:flex-row gap-20 pb-20`}>
-      <div className={"w-full lg:flex-1"}>
-        <div className="inline-flex flex-col items-start gap-2 relative flex-[0_0_auto]">
-          <div className="relative w-fit mt-[-1.00px] text-[48px] heading-l-regular font-[number:var(--heading-l-regular-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-regular-font-size)] tracking-[var(--heading-l-regular-letter-spacing)] leading-[var(--heading-l-regular-line-height)] whitespace-nowrap [font-style:var(--heading-l-regular-font-style)]">
-            Quiz Completed
-          </div>
-          <div className="relative w-fit heading-l-bold text-[48px] font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-bold-font-size)] tracking-[var(--heading-l-bold-letter-spacing)] leading-[var(--heading-l-bold-line-height)] whitespace-nowrap [font-style:var(--heading-l-bold-font-style)]">
-            Your Score...
+    <Suspense>
+      <div className={`w-full flex flex-col lg:flex-row gap-20 pb-20`}>
+        <div className={"w-full lg:flex-1"}>
+          <div className="inline-flex flex-col items-start gap-2 relative flex-[0_0_auto]">
+            <div className="relative w-fit mt-[-1.00px] text-[48px] heading-l-regular font-[number:var(--heading-l-regular-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-regular-font-size)] tracking-[var(--heading-l-regular-letter-spacing)] leading-[var(--heading-l-regular-line-height)] whitespace-nowrap [font-style:var(--heading-l-regular-font-style)]">
+              Quiz Completed
+            </div>
+            <div className="relative w-fit heading-l-bold text-[48px] font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-bold-font-size)] tracking-[var(--heading-l-bold-letter-spacing)] leading-[var(--heading-l-bold-line-height)] whitespace-nowrap [font-style:var(--heading-l-bold-font-style)]">
+              Your Score...
+            </div>
           </div>
         </div>
-      </div>
 
-      <div
-        className={
-          "w-full h-auto lg:flex-1  flex items-center justify-center flex-col"
-        }
-      >
-        <div className="w-full min-h-[250px] bg-white rounded-3xl flex flex-col items-center justify-center gap-8 relative flex-[0_0_auto]">
-        <div className={` flex items-center justify-center gap-8`}>
-          <div
-            className={`relative w-10 h-10  bg-[100%_100%] flex items-center justify-center`}
-            style={bgStyle}
-          >
-            <Image
-              width={10}
-              height={10}
-              className=" w-6 h-6"
-              alt="Fluent paint brush"
-              src={`${quiz.iconPath}`}
-            />
-          </div>
-          <div
-            className="relative w-fit  text-[#313e51] whitespace-nowrap font-medium
+        <div
+          className={
+            "w-full h-auto lg:flex-1  flex items-center justify-center flex-col"
+          }
+        >
+          <div className="w-full min-h-[250px] bg-white rounded-3xl flex flex-col items-center justify-center gap-8 relative flex-[0_0_auto]">
+            <div className={` flex items-center justify-center gap-8`}>
+              <div
+                className={`relative w-10 h-10  bg-[100%_100%] flex items-center justify-center`}
+                style={bgStyle}
+              >
+                <Image
+                  width={10}
+                  height={10}
+                  className=" w-6 h-6"
+                  alt="Fluent paint brush"
+                  src={`${quiz.iconPath}`}
+                />
+              </div>
+              <div
+                className="relative w-fit  text-[#313e51] whitespace-nowrap font-medium
       text-heading-body-m lg:text-heading-s"
-          >
-            {quiz.title}
+              >
+                {quiz.title}
+              </div>
+            </div>
+            <div className="relative w-fit heading-l-bold text-[64px] font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-bold-font-size)] tracking-[var(--heading-l-bold-letter-spacing)] leading-[var(--heading-l-bold-line-height)] whitespace-nowrap [font-style:var(--heading-l-bold-font-style)]">
+              {quizParams.score}
+            </div>
+            <p className="relative w-fit text-[24px] font-[number:var(--body-s-font-weight)] text-[#626c7f] lg:text-[length:var(--body-s-font-size)] tracking-[var(--body-s-letter-spacing)] leading-[var(--body-s-line-height)] ">
+              out of {quizParams.totalQuestions}{" "}
+            </p>
           </div>
+          <Link href={"/"} className="w-full">
+            <Button name={"Play Again"} />
+          </Link>
         </div>
-          <div className="relative w-fit heading-l-bold text-[64px] font-[number:var(--heading-l-bold-font-weight)] text-[#313e51] lg:text-[length:var(--heading-l-bold-font-size)] tracking-[var(--heading-l-bold-letter-spacing)] leading-[var(--heading-l-bold-line-height)] whitespace-nowrap [font-style:var(--heading-l-bold-font-style)]">
-            {quizParams.score}
-          </div>
-          <p className="relative w-fit text-[24px] font-[number:var(--body-s-font-weight)] text-[#626c7f] lg:text-[length:var(--body-s-font-size)] tracking-[var(--body-s-letter-spacing)] leading-[var(--body-s-line-height)] ">out of {quizParams.totalQuestions} </p>
-        </div>
-        <Link href={"/"} className="w-full">
-          <Button name={"Play Again"} />
-        </Link>
       </div>
-    </div>
+    </Suspense>
   );
 }
